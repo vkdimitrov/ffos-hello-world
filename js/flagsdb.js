8dbd0a2af54943100333318319042094e80275f4
Vladimir <dimitrov@vladko.org>
2014-01-13 21:46:57 +0200

    Delete flagsdb.js

--- js/flagsdb.js
+++
@@ -1,9 +0,0 @@
-var db = 
-{
-	"countries":
-	[
-		{"id": "1", "name": "Bulgaria", "flag": "flag.gif"},
-		{"id": "2", "name": "Macedonia", "flag": "flag2.gif"}
-		{"id": "3", "name": "Germany", "flag": "flag3.gif"}
-	]
-}
