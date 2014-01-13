a0f55bceebba3e10a94bf2e3835f4fa36f5674e4
Vladimir Dimitrov <vkdimitrov@vjsoft.net>
2014-01-13 21:42:21 +0200

    icon128 change

---
+++ js/flagsdb.js
@@ -0,0 +1,9 @@
+var db = 
+{
+	"countries":
+	[
+		{"id": "1", "name": "Bulgaria", "flag": "flag.gif"},
+		{"id": "2", "name": "Macedonia", "flag": "flag2.gif"}
+		{"id": "3", "name": "Germany", "flag": "flag3.gif"}
+	]
+}
